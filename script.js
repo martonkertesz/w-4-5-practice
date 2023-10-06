18066d79f142ceca48ae96963a9910a79e49113d
martonkertesz <kertesz.marton1992@gmail.com>
2023-10-06 20:41:01 +0200

    foreach

--- script.js
+++ script.js
@@ -1,23 +1,48 @@
-const arr = [1, 2, 3, 4, 5]
+const data = [
+    {
+        name: "Kovacs Janos",
+        age: 29,
+        subjects: [
+            {
+                subjectName: "Maths",
+                subjectGrade: 5
+            },
+            {
+                subjectName: "Physics",
+                subjectGrade: 3
+        },
+        {
+            subjectName: "English",
+            subjectGrade: 2
+        }
+        ]
+    }
+    ame: "Lefold Elek",
+        age: 91,
+        subjects: [
+            {
+                subjectName: "Maths",
+                subjectGrade: 2
+            },
+            {
+                subjectName: "Physics",
+                subjectGrade: 1
+        },
+        {
+            subjectName: "English",
+            subjectGrade: 5
+        }
+        ]
+    }
+]
 
-function returnDouble(number) {
-    return number * 2
+for (let i = 0; i < data.length; i++) {
+    console.log(data[i].name)
+    
 }
 
-const returnDouble2 = number => number * 2
+students.forEach(student =>  {
+console.log(student.name)
 
-console.log(returnDouble(25))
-for (let i = 0; i < arr.length; i++) {
-    console.log(returnDouble(arr[i]))
-}
-function logDouble(number) {
-    console.log(number * 2)
-}
-
-arr.forEach(function(element) {
-    console.log(element * 2)
+student.subjects.forEach(subject => console.log(`${subject.subjectName}: ${subject.subjectGrade}`))
 })
-
-arr.forEach(element => console.log(element * 2))
-
-arr.forEach(logDouble)
