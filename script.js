a563dd929d7914d5f55b83e1b193ab39d0c7a814
martonkertesz <kertesz.marton1992@gmail.com>
2023-10-27 13:57:24 +0200

    structure builtins

--- script.js
+++ script.js
@@ -1,66 +1,61 @@
-const studentsData = [
-    {
-        name: "Kovacs Janos",
-        age: 29,
-        subjects: [
-            {
-                subjectName: "Maths",
-                subjectGrade: 5
-            },
-            {
-                subjectName: "Physics",
-                subjectGrade: 3
-        },
-        {
-            subjectName: "English",
-            subjectGrade: 2
-        }
-        ]
-    }
-       {
-        name: "Lefold Elek",
-        age: 95,
-        subjects: [
-            {
-                subjectName: "Maths",
-                subjectGrade: 2
-            },
-            {
-                subjectName: "Physics",
-                subjectGrade: 1
-        },
-        {
-            subjectName: "English",
-            subjectGrade: 5
-        }
-        ]
-    }
-    {
-        name: "Kis Peter Elek",
-        age: 16,
-        subjects: [
-            {
-                subjectName: "Maths",
-                subjectGrade: 2
-            },
-            {
-                subjectName: "Physics",
-                subjectGrade: 5
-        },
-        {
-            subjectName: "English",
-            subjectGrade: 4
-        }
-        ]
-    }
-]
+/* const arr = ["egy", "ketto", "harom", "negy"]
 
-for (let i = 0; i < studentsData.length; i++) {
-    console.log(studentsData[i].name)
-    
+console.log(arr.includes("ketto"))
+
+console.log(["one", "two", "three", "four"].includes("four"))
+
+const obj = {
+    key1: "value",
+    key2: 10,
+    key3: function() {
+        console.log("hello")
+    }
+}
+console.log(obj.key3())
+ */
+
+
+/* const str = "hello world"
+
+console.log(str.substring(0, 3)) //hel
+
+console.log(str.charAt(4)) //o
+
+console.log(str.indexOf("world")) // 6 (hanyas indexnel kezdodik a keresett kifejezes)
+
+console.log(str.split(" ")) // ['hello', 'world']
+
+console.log(str.toUpperCase()) // HELLO WORLD
+
+console.log(str.toLocaleLowerCase())   */
+
+//console.log(Math.floor(Math.random() * 40 + 10)) // 10 es 40 kozott general random lefele kerekitett szamot
+
+/* const arr = ["alma", "korte", "uborka"]
+//arr.push("szilva", "birs", "szolo") //berakja ar array vegebe az uj elemeket
+arr.push({
+    key: "value"
+})
+console.log(arr)
+
+console.log(arr.pop()) //kiveszi az array utolso elemet; pop() -> kiveszi az elso elemet es visszaadja
+ 
+console.log(arr.reverse()) // megforditja az array elemeinek sorrendjet
+
+console.log(arr.join("")) //az array elemeit egymas melle irja, koztuk a megadott karakterrel */
+
+
+const obj = {
+    name: "Gipsz Jakab",
+    age: 27,
+    pets: [
+        "Mirci",
+        "Lajcsi",
+        "Geza"
+    ]
 }
 
-studentsData.forEach(student =>  {
-console.log(student.name)
-student.subjects.forEach(subject => console.log(`${subject.subjectName}: ${subject.subjectGrade}`))
-})
+for (const key of Object.keys(obj)) { //adott kulcs objectjein igy tudunk vegig iteralni; megkapom az osszes kulcs nevet es erteket
+    console.log(key, ":", obj[key])
+}
+console.log(Object.entries(obj))
